17be59b6884bf557b103124a35371704bdd57b12
Shoaib Ali Asif <133796092+Shoaib-Ali-ASif@users.noreply.github.com>
2023-07-16 17:47:29 +0500

    updated

--- show-books.php
+++ show-books.php
@@ -4,37 +4,67 @@
 <?php require_once('./includes/head.php'); ?>
 
 <body>
-  <div class="wrapper">
+    <div class="wrapper">
 
-    <?php require_once('./includes/sidenavbar.php'); ?>
+        <?php require_once('./includes/sidenavbar.php'); ?>
 
-    <div class="main">
+        <div class="main">
 
-      <?php require_once('./includes/topnavbar.php'); ?>
+            <?php require_once('./includes/topnavbar.php'); ?>
 
-      <main class="content">
-        <div class="container-fluid p-0">
-          <h1 class="h3 mb-3">Dasboard</h1>
+            <main class="content mx-5">
+                <div class="container-fluid p-0">
+                    <div class="row">
+                        <div class="col-6">
+                            <h1 class="h1 mb-3">Books</h1>
+                        </div>
+                        <div class="col-6 text-end">
+                            <a href="./add-book.php" class="btn btn-outline-primary">Add Book</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <table class="table table-bordered">
 
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h5 class="card-title mb-0">Own Content</h5>
+                                        <thead>
+                                            <tr>
+                                                <th>Sr. No.</th>
+                                                <th>Title</th>
+                                                <th>Author</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Hello</td>
+                                                <td>Me</td>
+                                                <td>
+                                                    <a href="" class="btn btn-secondary">Read-More</a>
+                                                    <a href="" class="btn btn-primary">Edit</a>
+                                                    <a href="" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+
+                                        </tbody>
+
+
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body"></div>
-              </div>
-            </div>
-          </div>
+            </main>
+            <?php require_once('./includes/footer.php'); ?>
+
         </div>
-      </main>
-
-      <?php require_once('./includes/footer.php'); ?>
-
     </div>
-  </div>
 
-  <script src="assets/js/app.js"></script>
+    <script src="assets/js/app.js"></script>
 </body>
 
 </html>
